6cc95bb93b582dba536f65556e394c9b1c9974f7
RomanHyde <romanhyde1@gmail.com>
2021-06-19 22:34:30 +0100

    add aync await and basic styling

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [data, setData] = useState({});
+
+
+  useEffect(() => {
+    handleFetch();
+  }, []);
+
+
+
+    useEffect(() => {
+      console.log()
+      
+    })
+  const handleFetch = async () => {
+    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    const data = await response.json();
+    setData(data);
+  };
+
+  return (
+    <div>
+      <h1>Chuck Norris Quote Machine</h1>
+      <button onClick={handleFetch}>handleFetch</button>
+      {data.value && (
+        <>
+          <h2>Joke:</h2>
+          <p>{data.value}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
